f39e45ff3bca682f83b598c7e80dea34666e4138
Mavbraz <18542083+Mavbraz@users.noreply.github.com>
2025-11-15 20:33:38 -0300

    Update unit test

--- packages/app/test/specs/routes-create-delete.spec.ts
+++ packages/app/test/specs/routes-create-delete.spec.ts
@@ -51,7 +51,7 @@ describe('Create and delete routes', () => {
     it('should add routes', async () => {
       await routes.addCRUDRoute();
       await routes.addHTTPRoute();
-      await routes.assertCount(1);
+      await routes.assertCount(2);
     });
 
     it('should switch to CALLBACKS tab', async () => {
@@ -69,68 +69,30 @@ describe('Create and delete routes', () => {
     });
   });
 
-  // describe('Delete route with tab reset from HTTP CALLBACKS to WebSocket RESPONSE', () => {
-  //   it('should create a new environment', async () => {
-  //     await environments.localAdd('test-env-websocket');
-  //     await routes.addHTTPRoute();
-  //     await routes.addHTTPRoute();
-  //     await routes.addHTTPRoute();
-  //   });
+  describe('Delete route with tab reset from HTTP CALLBACKS to WebSocket RESPONSE', () => {
+    it('should create a new environment', async () => {
+      await environments.localAdd('test-env-websocket');
+      await routes.remove(1);
+    });
 
-  //   it('should add a WebSocket route', async () => {
-  //     await routes.addWebSocketRoute();
-  //     await routes.assertCount(4);
-  //     await routes.setPath('test-ws');
-  //     await routes.assertMenuEntryText(4, '/test-ws\nWS');
-  //   });
+    it('should add routes', async () => {
+      await routes.addWebSocketRoute();
+      await routes.addHTTPRoute();
+      await routes.assertCount(2);
+    });
 
-  //   it('should select first HTTP route and switch to CALLBACKS tab', async () => {
-  //     await routes.select(1);
-  //     await routes.switchTab('CALLBACKS');
-  //     await routes.assertActiveRouteResponseTab('CALLBACKS');
-  //   });
+    it('should select first HTTP route and switch to CALLBACKS tab', async () => {
+      await routes.switchTab('CALLBACKS');
+      await routes.assertActiveRouteResponseTab('CALLBACKS');
+    });
 
-  //   it('should delete the HTTP route and verify tab is reset to RESPONSE', async () => {
-  //     await utils.dropdownMenuClick(
-  //       `.routes-menu .nav-item:nth-child(1) .nav-link`,
-  //       DropdownMenuRouteActions.DELETE,
-  //       true
-  //     );
-  //     await routes.assertCount(3);
-  //     await browser.pause(500);
-  //     await routes.assertActiveRouteResponseTab('RESPONSE');
-  //   });
+    it('should delete the HTTP route and verify tab is reset to RESPONSE', async () => {
+      await routes.remove(2);
+      await routes.assertCount(1);
+    });
 
-  //   it('should verify the WebSocket route does not have CALLBACKS tab', async () => {
-  //     await routes.select(3);
-  //     await routes.assertMenuEntryText(3, '/test-ws\nWS');
-  //     await routes.assertActiveRouteResponseTab('RESPONSE');
-  //     const callbacksTab = await $(
-  //       '#route-responses-menu .nav.nav-tabs .nav-item:nth-child(4) .nav-link'
-  //     );
-  //     expect(await callbacksTab.isExisting()).toEqual(false);
-  //   });
-
-  //   it('should verify the WebSocket route does not have HEADERS tab', async () => {
-  //     await routes.assertMenuEntryText(3, '/test-ws\nWS');
-  //     const headersTab = await $(
-  //       '#route-responses-menu .nav.nav-tabs .nav-item:nth-child(2) .nav-link'
-  //     );
-  //     expect(await headersTab.isExisting()).toEqual(false);
-  //   });
-
-  //   it('should verify HTTP route still has CALLBACKS tab', async () => {
-  //     await routes.select(1);
-  //     await routes.switchTab('CALLBACKS');
-  //     await routes.assertActiveRouteResponseTab('CALLBACKS');
-  //   });
-
-  //   it('should select WebSocket and verify tab is reset from CALLBACKS', async () => {
-  //     await routes.assertActiveRouteResponseTab('CALLBACKS');
-  //     await routes.select(3);
-  //     await routes.assertMenuEntryText(3, '/test-ws\nWS');
-  //     await browser.pause(300);
-  //     await routes.assertActiveRouteResponseTab('RESPONSE');
-  //   });
-  // });
+    it('should verify the WebSocket route does not have CALLBACKS tab', async () => {
+      await routes.assertActiveRouteResponseTab('RESPONSE');
+    });
+  });
 });
